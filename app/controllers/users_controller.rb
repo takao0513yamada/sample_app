18b2f3b4e9c5b96b9632f69446789bc9da871d2c
taka president <life.i.s.w---@i.softbank.jp>
2019-11-19 22:16:46 +0900

    Finish ch10

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,12 @@
 class UsersController < ApplicationController
+  before_action :logged_in_user, only: [:index, :edit, :update, :destroy]
+  before_action :correct_user,   only: [:edit, :update]
+  before_action :admin_user,     only: :destroy
+
+  def index
+   @users = User.paginate(page: params[:page])
+  end
+
   # GET /users/:id
   def show
     @user = User.find(params[:id])
@@ -6,6 +14,7 @@ class UsersController < ApplicationController
     # debugger
   end
 
+  # GET /users/new
   def new
     @user = User.new
   end
@@ -30,9 +39,64 @@ class UsersController < ApplicationController
     end
   end
 
+  # GET /users/:id/edit
+  # params[:id] => :id
+  def edit
+    @user = User.find(params[:id])
+    # => app/views/users/edit.html.erb
+  end
+
+  # PATCH /users/:id
+  def update
+    @user = User.find(params[:id])
+    if @user.update_attributes(user_params)
+      flash[:success] = "Profile updated"
+      redirect_to @user
+      # Success
+    else
+      # Failure
+     render 'edit'
+    # => @user.errors.full_messages()
+    end
+  end
+
+  # DELETE /users/:id
+  def destroy
+    User.find(params[:id]).destroy
+    flash[:success] = "User deleted"
+    redirect_to users_url
+  end
+
+  private
   def user_params
     params.require(:user).permit(
       :name, :email, :password,
       :password_confirmation)
+  end
+
+  # beforeアクション
+
+  # ログイン済みユーザーかどうか確認
+  def logged_in_user
+    unless logged_in?
+      # GET   /users/:id/edit
+      # PATCH /users/:id
+      store_location
+      flash[:danger] = "Please log in."
+      redirect_to login_url
+    end
+  end
+
+  # 正しいユーザーかどうか確認
+  def correct_user
+    # GET   /users/:id/edit
+    # PATCH /users/:id
+    @user = User.find(params[:id])
+    redirect_to(root_url) unless current_user?(@user)
+  end
+
+  # 管理者かどうか確認
+  def admin_user
+    redirect_to(root_url) unless current_user.admin?
   end
 end
